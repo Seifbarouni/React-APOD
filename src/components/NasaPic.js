ddcce49a8ed0825820899ecaa40ce10a106c9c9c
Seifbarouni <seif.barouni11@gmail.com>
2021-01-01 20:40:41 +0100

    key problem fixed

--- src/components/NasaPic.js
+++ src/components/NasaPic.js
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom'
 import {useState,useEffect} from 'react'
 import Pic from './Pic'
 import Loading from './Loading'
-const apiKey = process.env.REACT_APP_NASA_API_KEY;
+//const apiKey = process.env.REACT_APP_NASA_API_KEY;
 
 const NasaPic = () => {
     const [picdata,setPicdata] = useState([])
@@ -11,7 +11,7 @@ const NasaPic = () => {
     useEffect(() => {
         const fetchData =async ()=>{
             setLoading(true)
-            const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&hd=true`)
+            const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=gA6wb0nPTfM97ahuZaHbrEDNMgwQ5GgkNHRpOh6b&hd=true`)
             const data = await res.json()
             setPicdata(data)
             setLoading(false)
