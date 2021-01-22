19eeb5a55ad84a134dbcdee6ea4f74cba7017608
Seifbarouni <seif.barouni11@gmail.com>
2021-01-22 19:05:16 +0100

    added redirects

--- src/components/NasaPic.js
+++ src/components/NasaPic.js
@@ -1,17 +1,17 @@
-import {Link} from 'react-router-dom'
-import {useState,useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Pic from './Pic'
 import Loading from './Loading'
-//const apiKey = process.env.REACT_APP_NASA_API_KEY;
+const apiKey = process.env.REACT_APP_NASA_API_KEY;
 
 const NasaPic = () => {
-    const [picdata,setPicdata] = useState([])
-    const [loading,setLoading] = useState(false)
+    const [picdata, setPicdata] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        const fetchData =async ()=>{
+        const fetchData = async () => {
             setLoading(true)
-            const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=gA6wb0nPTfM97ahuZaHbrEDNMgwQ5GgkNHRpOh6b&hd=true`)
+            const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&hd=true`)
             const data = await res.json()
             setPicdata(data)
             setLoading(false)
@@ -20,13 +20,13 @@ const NasaPic = () => {
     }, [])
     return (
         <>
-        <Link to="/" className="linkHome">Home</Link>
+            <Link to="/" className="linkHome">Home</Link>
 
-        <div className="NasaPic">
+            <div className="NasaPic">
 
-            {!loading ?<Pic picdata={picdata}/> : <Loading>Loading...</Loading>}
+                {!loading ? <Pic picdata={picdata} /> : <Loading>Loading...</Loading>}
 
-        </div>
+            </div>
         </>
     )
 }
